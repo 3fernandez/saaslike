a501ce88de516a0ca152d3e2499ce5b5ccee21a2
Euclides Fernandes <ef.webdev@gmail.com>
2016-05-22 15:43:15 +0100

    Add all the relationships between models.

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,7 @@
 class Account < ActiveRecord::Base
+  has_many :memberships, dependent: :destroy
+  has_many :users, through: :memberships
+
   validates :name, :subdomain, presence: true
   validates :subdomain, format: { with: /\A[a-z][a-z0-9\-]+[a-z0-9]\Z/ }, uniqueness: true
 end
